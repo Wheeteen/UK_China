8a18e7d1c0272edf07e23c89bf5fbc0b4cce2efd
wheeteen <1970234657@qq.com>
2018-06-10 17:50:29 +0800

    test1

---
+++ static/js/index.js
@@ -0,0 +1,14 @@
+(function(){
+	var vm = new Vue({
+       el: 'body',
+       data:{
+       	 
+       },
+       computed: function(){
+
+       },
+       methods: {
+     
+       }
+	});
+})();
